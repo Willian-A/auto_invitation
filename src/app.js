150d04f58889016279c37a042cd422ee1e035ab8
Willian Almeida <willian.contatoal@gmail.com>
2022-04-18 16:33:55 -0300

    corrigindo o fundo branco nas imagens

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {
   exportComponentAsJPEG,
-  exportComponentAsPDF,
   exportComponentAsPNG,
 } from "react-component-export-image";
 import { useCSVReader } from "react-papaparse";
@@ -23,67 +22,64 @@ function ComponenteImage(props) {
     const componentRef = React.useRef();
     // array.push({ ref: componentRef, nome: texto[0].trim() });
     array.push(
-      <div key={index}>
-        <div ref={componentRef} className="container">
-          <img src={fundo_grade} alt="" className="fundo" />
-          <img src={logo_iniciativa} alt="" className="logo" />
-          <div className="barras">
-            <div className="barra"></div>
-            <h4>fisweek</h4>
-            <div className="barra"></div>
-          </div>
-          <b>
-            <h1>CONVITE</h1>
-          </b>
-          <h3>
-            {texto[2].toLowerCase() === "masculino" ? "Prezado" : "Prezada"}{" "}
-            <b>{texto[0].trim()},</b>
-          </h3>
-          <h3>
-            24 meses depois,{" "}
-            <b className="fru_azul">
-              VOCÊ <img src={fru_azul} alt="" />
-            </b>
-            <br /> é{" "}
-            {texto[2].toLowerCase() === "masculino"
-              ? "nosso convidado"
-              : "nossa convidada"}{" "}
-            para o grande reencontro da Saúde.
-          </h3>
-          <div className="blue_hour">
-            <img src={forma} alt="" className="forma" />
-            <b className="fru_branco">
-              <img src={fru_branco} alt="" />
-              <h2>BLUE HOUR</h2>
-            </b>
-            <h2>
-              o momento em que os <br />
-              grandes da Saúde se reencontram
-            </h2>
-            <h4>
-              O espaço projetado para funcionar no centro do Rio de Janeiro foi
-              preparado para promover inteira- cões e conexões{" "}
-              <b>presenciais</b> entre as lideranças, parceiros, empreendedores
-              e startups.
-            </h4>
-            <h4 className="small">
-              <b>Endereço:</b> Casa do Empresário Rua da Candelária 9, Mezanino
-            </h4>
-            <b>
-              <h4>Venha viver conosco esta experiência!</h4>
-            </b>
-            <div className="data">
-              <b>
-                <h2>{texto[1]}</h2>
-              </b>
-              <b>
-                <h2>16:00 às 21:00</h2>
-              </b>
-            </div>
-            <h5>RSVP {texto[3].toLowerCase()} para este número.</h5>
-          </div>
-          <img src={powered_by} alt="" className="powered_by" />
+      <div ref={componentRef} className="container" key={index}>
+        <img src={fundo_grade} alt="" className="fundo" />
+        <img src={logo_iniciativa} alt="" className="logo" />
+        <div className="barras">
+          <div className="barra"></div>
+          <h4>fisweek</h4>
+          <div className="barra"></div>
         </div>
+        <b>
+          <h1>CONVITE</h1>
+        </b>
+        <h3>
+          {texto[2].toLowerCase() === "masculino" ? "Prezado" : "Prezada"}{" "}
+          <b>{texto[0].trim()},</b>
+        </h3>
+        <h3>
+          24 meses depois,{" "}
+          <b className="fru_azul">
+            VOCÊ <img src={fru_azul} alt="" />
+          </b>
+          <br /> é{" "}
+          {texto[2].toLowerCase() === "masculino"
+            ? "nosso convidado"
+            : "nossa convidada"}{" "}
+          para o grande reencontro da Saúde.
+        </h3>
+        <div className="blue_hour">
+          <img src={forma} alt="" className="forma" />
+          <b className="fru_branco">
+            <img src={fru_branco} alt="" />
+            <h2>BLUE HOUR</h2>
+          </b>
+          <h2>
+            o momento em que os <br />
+            grandes da Saúde se reencontram
+          </h2>
+          <h4>
+            O espaço projetado para funcionar no centro do Rio de Janeiro foi
+            preparado para promover inteira- cões e conexões <b>presenciais</b>{" "}
+            entre as lideranças, parceiros, empreendedores e startups.
+          </h4>
+          <h4 className="small">
+            <b>Endereço:</b> Casa do Empresário Rua da Candelária 9, Mezanino
+          </h4>
+          <b>
+            <h4>Venha viver conosco esta experiência!</h4>
+          </b>
+          <div className="data">
+            <b>
+              <h2>{texto[1]}</h2>
+            </b>
+            <b>
+              <h2>16:00 às 21:00</h2>
+            </b>
+          </div>
+          <h5>RSVP {texto[3].toLowerCase()} para este número.</h5>
+        </div>
+        <img src={powered_by} alt="" className="powered_by" />
       </div>
     );
     return {
@@ -99,11 +95,14 @@ function ComponenteImage(props) {
         onClick={(event) => {
           event.target.disabled = true;
           teste.forEach((value) => {
-            exportComponentAsJPEG(value.ref, { fileName: value.nome });
+            exportComponentAsPNG(value.ref, {
+              fileName: value.nome,
+              html2CanvasOptions: { backgroundColor: null },
+            });
           });
         }}
       >
-        Exportar tudo como JPEG
+        Exportar tudo como PNG
       </button>
       {array}
     </>
